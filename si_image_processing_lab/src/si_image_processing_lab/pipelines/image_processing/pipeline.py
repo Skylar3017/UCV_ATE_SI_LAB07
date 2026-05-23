e4be155bddc97d9ba7dc87993fb20001f338bc8a
Luciana Palomino <lpalominoti17@ucvvirtual.edu.pe>
2026-05-23 01:53:06 -0500

    Fix Ruff lint errors

--- si_image_processing_lab/src/si_image_processing_lab/pipelines/image_processing/pipeline.py
+++ si_image_processing_lab/src/si_image_processing_lab/pipelines/image_processing/pipeline.py
@@ -1,18 +1,15 @@
 from kedro.pipeline import Pipeline, node
+
 from .nodes import process_image
 
+
 def create_pipeline(**kwargs):
+
     return Pipeline(
         [
             node(
                 func=process_image,
-                inputs=[
-                    "params:input_path",
-                    "params:output_path",
-                    "params:rotation_angle",
-                    "params:filter_name",
-                    "params:watermark_text",
-                ],
+                inputs=["params:input_path", "params:output_path"],
                 outputs="processed_image",
                 name="process_image_node",
             )
